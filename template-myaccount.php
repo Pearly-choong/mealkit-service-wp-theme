d7eb6bc49bbff1da5dd1271f178b7ca96f921608
Pearly-choong <choongpl@gmail.com>
2020-06-18 19:58:42 +1200

    fixed woocommerce form responsiveness

--- template-myaccount.php
+++ template-myaccount.php
@@ -6,33 +6,22 @@ Template Name: MyAccount
 
 <?php get_header(); ?>
 
-<!--
-<section class="page-wrap">-->
-<div class="container">
-
-  <section class="row">
-
-    <div class="">
-
+<section class="page-wrap mt-5">
+  <div class="container">
       <h1> <?php the_title(); ?>   </h1>
 
       <?php //if(has_post_thumbnail()): ?>
-          <!-- This has a featured image -->
-          <!-- <div>
-            <img src="<?php //the_post_thumbnail_url('blog-large'); ?>" alt="<?php// the_title(); ?>" class="mb-3 img-fluid img-thumbnail">
-          </div> -->
-    <?php //endif; ?>
+            <!-- This has a featured image -->
+            <!-- <div>
+              <img src="<?php //the_post_thumbnail_url('blog-large'); ?>" alt="<?php// the_title(); ?>" class="mb-3 img-fluid img-thumbnail">
+            </div> -->
+      <?php //endif; ?>
 
-    <div><?php
-      //the_content();
-      ?>
-      <?php get_template_part('includes/section','content'); ?>
-    </div>
-
-    </div>
-
-
-  </section>
-
-</div>
+      <div>
+        <div class="container my-height">
+        <?php get_template_part('includes/section','content'); ?>
+        </div>
+      </div>
+  </div>
+</section>
 <?php get_footer(); ?>
